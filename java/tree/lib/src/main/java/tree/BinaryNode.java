914d48e4428b5bc61ad9acc9aab14506db307a68
Adrian Butler Jr <butlerjr123@gmail.com>
2022-11-14 16:41:47 -0600

    started on binary tree

---
+++ java/tree/lib/src/main/java/tree/BinaryNode.java
@@ -0,0 +1,38 @@
+package tree;
+
+public class BinaryNode<T>
+{
+  private T value;
+  private BinaryNode<T> left;
+  private BinaryNode<T> right;
+
+  public T getValue()
+  {
+    return value;
+  }
+
+  public void setValue(T value)
+  {
+    this.value = value;
+  }
+
+  public BinaryNode<T> getLeft()
+  {
+    return left;
+  }
+
+  public void setLeft(BinaryNode<T> left)
+  {
+    this.left = left;
+  }
+
+  public BinaryNode<T> getRight()
+  {
+    return right;
+  }
+
+  public void setRight(BinaryNode<T> right)
+  {
+    this.right = right;
+  }
+}
